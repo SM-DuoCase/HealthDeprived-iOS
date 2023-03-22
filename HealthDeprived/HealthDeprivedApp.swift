a5358147d40a6eb8cb29167d754e4e7118215768
Josian van Efferen <github@josian.nl>
2023-03-22 13:04:47 +0100

    Initial Commit

---
+++ HealthDeprived/HealthDeprivedApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDeprivedApp.swift
+//  HealthDeprived
+//
+//  Created by Josian van Efferen on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthDeprivedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
