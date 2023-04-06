6d8c7914883ee3aa50cb486565dfbccb161905b8
Josian van Efferen <github@Josian.nl>
2023-04-06 09:13:57 +0200

    Start logic

--- HealthDeprived/HealthDeprivedApp.swift
+++ HealthDeprived/HealthDeprivedApp.swift
@@ -6,26 +6,80 @@
 //
 
 import SwiftUI
+import HealthKit
 
 @main
 struct HealthDeprivedApp: App {
+    
+    private let healthStore: HKHealthStore
+    private let factory: Factory
+        
+    init() {
+        guard HKHealthStore.isHealthDataAvailable() else {  fatalError("This app requires a device that supports HealthKit") }
+        healthStore = HKHealthStore()
+        factory = Factory()
+        factory.generateClasses(_healthStore: healthStore)
+        requestHealthkitPermissions()
+    }
+    
+    private func requestHealthkitPermissions() {
+        
+        let sampleTypesToRead = Set([
+            HKObjectType.characteristicType(forIdentifier: .biologicalSex)!,
+            HKObjectType.characteristicType(forIdentifier: .dateOfBirth)!,
+            HKObjectType.categoryType(forIdentifier: .appleStandHour)!,
+            HKObjectType.quantityType(forIdentifier: .appleExerciseTime)!,
+            HKObjectType.quantityType(forIdentifier: .appleMoveTime)!,
+            HKObjectType.quantityType(forIdentifier: .height)!,
+            HKObjectType.quantityType(forIdentifier: .bodyMass)!,
+            HKObjectType.quantityType(forIdentifier: .heartRate)!,
+            HKObjectType.categoryType(forIdentifier: .lowHeartRateEvent)!,
+            HKObjectType.categoryType(forIdentifier: .highHeartRateEvent)!,
+            HKObjectType.quantityType(forIdentifier: .restingHeartRate)!,
+            HKObjectType.quantityType(forIdentifier: .heartRateVariabilitySDNN)!,
+            HKObjectType.quantityType(forIdentifier: .respiratoryRate)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryCarbohydrates)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryFiber)!,
+            HKObjectType.quantityType(forIdentifier: .dietarySugar)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryFatTotal)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryProtein)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryVitaminA)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryVitaminB6)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryVitaminB12)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryVitaminC)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryVitaminD)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryVitaminE)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryVitaminK)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryCalcium)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryWater)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryCaffeine)!,
+            HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
+        ])
+        
+        healthStore.requestAuthorization(toShare: nil, read: sampleTypesToRead) { (success, error) in
+            print("Request Authorization -- Success: ", success, " Error: ", error ?? "nil")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             TabView {
-                HomeView()
+                HomeView(_factory: factory)
                     .tabItem {
                         Label("Home", systemImage: "house.fill")
                     }
                     .preferredColorScheme(.light)
-
+                
                 SettingsView()
                     .tabItem {
                         Label("Settings", systemImage: "slider.vertical.3")
                     }
                     .preferredColorScheme(.light)
-                
+                    //.environmentObject(healthStore)
             }
             
         }
     }
 }
+
+extension HKHealthStore: ObservableObject{}
